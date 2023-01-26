2b0aa174c77db047a820e6d900d0d7dbde31a61a
enrique palomino <octepc.97@gmail.com>
2023-01-26 12:05:03 -0500

    almost finished the calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  numb1:number = 0
+  numb2:number=0
+
+
+  options=[
+    {
+      name:'+',
+      value: '+',
+      selected:true
+    },
+    {
+      name:'-',
+      value: '-'
+    },
+    {
+      name:'x',
+      value: 'x'
+    },
+    {
+      name:'/',
+      value: '/'
+    }
+  ]
+
+  selectedOption=this.options[0].value
+
+  operationResult:number=0
+
+  calcular(selectedOption:string){
+    this.selectedOption= selectedOption
+
+    switch (selectedOption) {
+      case '+':
+          this.operationResult = Number(this.numb1) + Number(this.numb2);
+          break;
+      case '-':
+          this.operationResult = Number(this.numb1) - Number(this.numb2);
+          break;
+      case 'x':
+          this.operationResult = Number(this.numb1) * Number(this.numb2);
+          break;
+      case '/':
+          this.operationResult = Number(this.numb1) / Number(this.numb2);
+          break;
+  }
+
+
+  console.log(selectedOption)
+  console.log(this.operationResult)
+
+  }
+
 }
